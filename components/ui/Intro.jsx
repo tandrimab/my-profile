b3d1789d9f8b48a3918bcf2b5a54fe48cec12d6d
tandrimab <bhattacharjee.tan@outlook.com>
2024-08-29 02:41:43 +0530

    Implementation of Intro section with animation

--- components/ui/Intro.jsx
+++ components/ui/Intro.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Intro() {
   return (
     <div className="overflow-hidden h-[100vh]">
@@ -5,9 +7,17 @@ export default function Intro() {
         className="h-full w-full animate-bgAnimate content-center text-center bg-repeat-x backdrop-blur-sm"
         style={{ backgroundImage: "url('/assets/background/bg_top2.jpg')" }}
       >
-        <div className="text-[100px] content-center text-center h-[700px] max-w-[1200px] bg-[rgba(0,0,0,0.4)] mx-auto p-12 backdrop-blur-md rounded-[25px]">
-          <p>Hello, World!</p>
-          <p>I'm Tandrima</p>
+        <div className=" content-center text-center h-max max-w-[1200px] bg-[rgba(0,0,0,0.4)] lg:mx-auto backdrop-blur-md rounded-[25px] wrapper sm:m-4 p-16">
+            <Image
+                src="/assets/images/profile_pic.jpg"
+                width={300}
+                height={300}
+                className="rounded-full"
+            />
+          <div className="text-nowrap lg:text-[6rem] sm:text-[4rem]">{"<b>"}Hello, World!{"</b>"}</div>
+          <div className="typing lg:text-[6rem] sm:text-[4rem] md:text-[4rem]">
+            I'm <span className="text-blueText">Tandrima</span>
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       backgroundImage: {},
       colors: {
         jetBlack: "#121212",
+        blueText: "#63A1EF"
       },
       keyframes:{
         animatedBackground: {
@@ -19,10 +20,37 @@ module.exports = {
           "100%": {
             backgroundPosition: "100% 0"
           }
+        },
+        textTyping: {
+          "10%,15%,30%,35%,50%,55%,70%,75%,90%,95%": {
+            width: "0%"
+          },
+          "5%,20%,25%,40%,45%,60%,65%,80%,85%": {
+            width: "calc(100% + 8px)"
+          }
+        },
+        cursorBlink: {
+          "0%": {
+            cursorColor: 'transparent'
+          },
+          "100%": {
+            cursorColor: '#63a1ef'
+          }
+        },
+        changeWord: {
+          "0%,50%": {
+            content: "Software Developer"
+          },
+          "51%,100%": {
+            content: "Frontend Developer"
+          }
         }
       },
       animation: {
-        bgAnimate: 'animatedBackground 20s linear infinite'
+        bgAnimate: 'animatedBackground 20s linear infinite',
+        textyping: 'cursorBlink 0.8s infinite, textTyping 10s steps(14) infinite',
+        changeWord: 'changeWord 5s linear infinite',
+        cursorBlink: ''
       }
     },
   },
