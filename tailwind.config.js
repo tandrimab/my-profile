47034c4875d771db73480b1cf593c5053d66effc
tandrimab <bhattacharjee.tan@outlook.com>
2024-10-20 16:48:48 +0530

    Added screen sizes in tailwin.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px'
+    },
     extend: {
       colors: {
         background: "var(--background)",
