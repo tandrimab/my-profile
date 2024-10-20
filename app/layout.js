60f9e5258c784bfb0b7d5f62ba5905480663624c
tandrimab <bhattacharjee.tan@outlook.com>
2024-10-20 15:48:48 +0530

    FOnt configuration: Using Geist font developed by vercel

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,17 @@
 import "./globals.css";
+import localFont from "next/font/local";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -9,6 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
