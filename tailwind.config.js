dc88d30fb81abd230540b4c54ade45f815de824f
tandrimab <bhattacharjee.tan@outlook.com>
2024-10-21 10:06:47 +0530

    Modified screen sizes, added page color, added background image of the hero section, defined linear gradient color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,46 @@ module.exports = {
   ],
   theme: {
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '960px',
-      xl: '1200px'
+      sm: "100px",
+      md: "540px",
+      lg: "960px",
+      xl: "1200px",
     },
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        blue: "#4285f4",
+        violet: "#9b72cb",
+        brick: "#d96570",
+        dark: "#131313",
+      },
+
+      backgroundImage: (theme) => ({
+        gradient_text: `linear-gradient(74deg,${theme("colors.blue")} 0,${theme(
+          "colors.violet"
+        )} 9%,${theme("colors.brick")} 20%,${theme("colors.brick")} 24%,${theme(
+          "colors.violet"
+        )} 35%,${theme("colors.blue")} 44%,${theme(
+          "colors.violet"
+        )} 50%,${theme("colors.brick")} 56%,${theme("colors.dark")} 75%,${theme(
+          "colors.dark"
+        )} 100%)`,
+        hero: "url('/assets/bg_top.jpg')",
+      }),
+      keyframes: {
+        animatedBackground: {
+          "0%": {
+            backgroundPosition: "0 0",
+          },
+          "100%": {
+            backgroundPosition: "100% 0",
+          },
+        },
+      },
+      animation: {
+        bgAnimate: "animatedBackground 25s linear infinite",
       },
     },
   },
