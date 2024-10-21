344e6e12b7e2f623ab821faa73d92117b59ad9e0
tandrimab <bhattacharjee.tan@outlook.com>
2024-10-21 10:38:03 +0530

    Added constants that are rendered into the page

---
+++ app/constants/index.js
@@ -0,0 +1,10 @@
+export const hero = {
+    profiePic: '/assets/profile_pic.jpg',
+  intro: "Hello, World!",
+  nameText: {
+    field1: "I'm Tandrima",
+    field2: "Frontend Developer",
+  },
+};
+
+
