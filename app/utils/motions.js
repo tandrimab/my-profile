9e58262b4c093af4029637979547ce670c935c94
tandrimab <bhattacharjee.tan@outlook.com>
2024-10-21 10:39:34 +0530

    Added framer motion variants in a separate file

---
+++ app/utils/motions.js
@@ -0,0 +1,10 @@
+export const typewriterAnimation = (delay) => {
+    return {
+        hidden: {
+            opacity: 0
+        },
+        visible: {
+            opacity: 1,
+        }
+    }
+};
